7eb123440fe45f0191e1bca31dd6aaf53c1f0da2
jpreslov <lovett_jp@outlook.com>
2021-03-01 22:33:27 -0600

    copying Luke's styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Timeline from './components/Timeline';
+import Felix from './assets/Felix_the_cat.png';
+import PostIcon from './assets/post.png';
 
 const App = () => {
   const [newUsername, setNewUsername] = useState('');
@@ -8,50 +10,84 @@ const App = () => {
     {
       username: 'gatorsfan-40',
       message: 'I love that team',
+      createdAt: `${new Date('2021-02-27T05:30:15.284Z').toLocaleString()}`,
     },
     {
       username: 'bingcrosbyfan-3000',
       message: 'That teams not as good as my man Bing',
+      createdAt: `${new Date('2021-02-27T05:30:15.284Z').toLocaleString()}`,
     },
     {
       username: 'bigchillin-guy',
       message: 'Sometimes its good to just realx',
+      createdAt: `${new Date('2021-02-22T13:33:15.284Z').toLocaleString()}`,
     },
   ]);
 
   let submitChirp = () => {
-    let newChirp = {
-        username: newUsername,
-        message: newMsg
-    }
+    let d = new Date;
 
-      setChirps([...chirps, newChirp])
-      emptyInputs()
-  }
+    let newChirp = {
+      username: newUsername,
+      message: newMsg,
+      createdAt: d.toLocaleString(),
+    };
+
+    setChirps([...chirps, newChirp]);
+    emptyInputs();
+  };
 
   let emptyInputs = () => {
-      setNewUsername('')
-      setNewMsg('')
-  }
+    setNewUsername('');
+    setNewMsg('');
+  };
 
-  let myChirps = chirps.map((chirp, id) => <Timeline key={id} chirp={chirp}/>);
+  let myChirps = chirps.map((chirp, id) => <Timeline key={id} chirp={chirp} />);
 
   return (
     <>
-      <div className="row m-4">
+      <div className="row-lg-12 mb-4 p-3 bg-light">
         <h1>Chirpr</h1>
-        <div className="col-2">
-          <input type="text" id="un-input" value={newUsername} placeholder="Username" onChange={(e) => setNewUsername(e.target.value)}></input>
-        </div>
-        <div className="col-2">
-          <input type="text" id="msg-input"  value={newMsg} placeholder="What's on your mind?" onChange={(e) => setNewMsg(e.target.value)}></input>
-        </div>
-        <div className="col-2">
-            <button className="btn btn-primary" onClick={submitChirp}>Submit chirp</button>
-        </div>
       </div>
 
-      {myChirps}
+      <div className="row-md m-6 d-flex align-items-start">
+        <div id="input-box" className="col-md-3 p-4 bg-light shadow">
+          <div className="input-group input-group-sm">
+            <span className="input-group-text">
+              <img id="felix" src={Felix} alt="" />
+            </span>
+            <input
+              className="form-control"
+              type="text"
+              id="un-input"
+              value={newUsername}
+              placeholder="Username"
+              onChange={(e) => setNewUsername(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="input-group input-group-sm">
+            <span className="input-group-text">
+              <img id="post" src={PostIcon} alt="" />
+            </span>
+
+            <textarea
+              className="form-control"
+              type="text"
+              id="msg-input"
+              value={newMsg}
+              placeholder="What's on your mind?"
+              onChange={(e) => setNewMsg(e.target.value)}
+            ></textarea>
+          </div>
+          <button className="btn btn-primary" onClick={submitChirp}>
+            Submit chirp
+          </button>
+        </div>
+        <div id="chirp-box" className="col-md-4 rounded shadow">
+          {myChirps}
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 //destructuring the chirp prop, could also just say props.chirp without curlies
 const Timeline = ({ chirp }) => {
-    return (
-        <div className="card m-5">
-        <div className="card-body">
-          <h5 className="card-title">{chirp.username}</h5>
-          <p className="card-text">{chirp.message}</p>
-        </div>
+  return (
+    <div className="bg-light card border-0">
+      <div className="card-body">
+        <h5 className="card-title">{chirp.username}</h5>
+        <p className="card-text">{chirp.message}</p>
+        <p className="text-muted">{chirp.createdAt}</p>
+        <hr></hr>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Timeline
+export default Timeline;
